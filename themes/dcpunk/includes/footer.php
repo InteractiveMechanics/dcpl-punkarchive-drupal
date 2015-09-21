6ac1c54a2538da15a185c20b130a924084f29c79
Michael Tedeschi <mike.tedeschi@gmail.com>
2015-09-21 11:31:25 -0400

    gitignore issue, add includes folder

---
+++ themes/dcpunk/includes/footer.php
@@ -0,0 +1,28 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<ul class="footer-nav">
+					<li><a href="about.html">About the Archive</a></li>
+					<li><a href="contribute.html">Contribute to the Archive</a></li>
+					<li><a href="resources.html">Resources</a></li>
+					<li><a href="contact.html">Contact Us</a></li>
+				</ul>
+			</div> 		
+		</div> <!-- END ROW -->
+	</div> <!-- END CONTAINER -->
+	<div class="divider"></div>
+	<div class="container">
+		<div class="row">
+			<div class="address-block">
+				<p><a href="http://wwww.dclibrary.org"><img src="images/dcpl_logo.png" alt="DC Public Library logo" id="logo"></a></p>
+				<p class="copyright"><small>Copyright &copy; 2015 DC Public Library. All rights reserved.</small></p>
+				<ul>
+					<li><a href="http://dclibrary.org/"><small>DC Public Library</small></a></li>
+					<li><a href=""><small>Terms &amp; Conditions</small></a></li>
+					<li><a href=""><small>Accessiblity</small></a></li>
+				</ul>
+			</div>
+		</div> <!-- END ROW -->
+	</div> <!-- END CONTAINER -->
+</footer>
