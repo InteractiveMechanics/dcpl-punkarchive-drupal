b909690106018b5c6601359a173e3aed34c6df05
Christina Deemer <cahdeemer@gmail.com>
2016-03-25 16:37:56 -0400

    added map to custom.scss

--- themes/whitelabel/js/map.js
+++ themes/whitelabel/js/map.js
@@ -9,8 +9,8 @@ var map = L.map('map').setView([38.9, -77.015], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'cahdeemer.pecnjfnp', 
-    accessToken: 'pk.eyJ1IjoiY2FoZGVlbWVyIiwiYSI6ImIwYzM3OGYyZTBmYmZlMWJmMzQ5OTNmZWRjMTA3NjNmIn0.4lMKKySybu846ym7-BNbYA'
+    id: 'mapbox.streets', 
+    accessToken: 'pk.eyJ1IjoiaW50ZXJhY3RpdmVtZWNoIiwiYSI6InJlcUtqSk0ifQ.RUwHuEkBbXoJ6SgOnXmYFg'
 }).addTo(map);
 
 var dataSuccess = function(jsonData) {
